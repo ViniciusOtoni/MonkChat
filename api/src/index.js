29d9cd671ee362aa62c8eb809c0736e86f579d4d
Bruno de Oliveira <brunooliveira.me@outlook.com>
2021-08-18 10:36:54 -0300

    ok

--- api/src/index.js
+++ api/src/index.js
@@ -23,4 +23,4 @@ app.get('/chat/:salaId', async (req, resp) => {
 
 
 app.listen(process.env.PORT,
-           x => console.log(`Server up at port ${process.env.PORT}`))
+           x => console.log(`>> Server up at port ${process.env.PORT}`))
